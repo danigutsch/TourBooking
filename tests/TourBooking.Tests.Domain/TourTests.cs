3e958da23a56c0b8af60af366a57cda9eac6e6b1
danigutsch <daniutsch@outlook.com>
2025-06-24 18:10:55 +0200

    Add validation for Tour name length and implement DateTime extension method

--- tests/TourBooking.Tests.Domain/TourTests.cs
+++ tests/TourBooking.Tests.Domain/TourTests.cs
@@ -11,4 +11,35 @@ public class TourTests
 
         Assert.NotNull(tour);
     }
+
+    [Theory]
+    [InlineData(0, false)]
+    [InlineData(1, false)]
+    [InlineData(2, false)]
+    [InlineData(3, true)]
+    [InlineData(4, true)]
+    [InlineData(99, true)]
+    [InlineData(100, true)]
+    [InlineData(101, false)]
+    [InlineData(102, false)]
+    public void Name_Length_Has_To_Be_Between_3_And_100_Characters(int length, bool canCreate)
+    {
+        // Arrange
+        var name = new string('a', length);
+        var today = DateTime.UtcNow.ToDateOnly();
+        var endDate = today.AddDays(5);
+
+        // Act
+        var action = Record.Exception(() => new Tour(name, "A valid description", 100.0m, today, endDate));
+
+        // Assert
+        if (canCreate)
+        {
+            Assert.Null(action);
+        }
+        else
+        {
+            Assert.IsType<ArgumentException>(action);
+        }
+    }
 }
