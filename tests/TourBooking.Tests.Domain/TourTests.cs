e9da463b50686e7ac09de5bec281383d6f5e2a10
danigutsch <daniutsch@outlook.com>
2025-06-24 18:11:52 +0200

    Remove Can_Create_Tour test method from TourTests

--- tests/TourBooking.Tests.Domain/TourTests.cs
+++ tests/TourBooking.Tests.Domain/TourTests.cs
@@ -4,14 +4,6 @@ namespace TourBooking.Tests.Domain;
 
 public class TourTests
 {
-    [Fact]
-    public void Can_Create_Tour()
-    {
-        var tour = new Tour("Test Tour", "This is a test tour", 100.0m, DateOnly.MinValue, DateOnly.MaxValue);
-
-        Assert.NotNull(tour);
-    }
-
     [Theory]
     [InlineData(0, false)]
     [InlineData(1, false)]
