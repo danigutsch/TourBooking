5c77a086d5af4d5dd75fd52ac0145249842afa27
danigutsch <daniutsch@outlook.com>
2025-06-29 15:22:29 +0200

    Add partial class for Program in testing project
    This commit introduces a partial class declaration for `Program` in the `Program.Testing.cs` file. It includes compiler directives to suppress specific warnings, enabling the test project to access the `Program` class without requiring instantiation. This change facilitates more effective testing of the class's functionality.

---
+++ src/TourBooking.ApiService/Program.Testing.cs
@@ -0,0 +1,8 @@
+﻿#pragma warning disable S3903
+#pragma warning disable S1118
+// ReSharper disable once CheckNamespace
+// ReSharper disable once ClassNeverInstantiated.Global
+/// <summary>
+/// This is required to allow the test project to access the Program class.
+/// </summary>
+public partial class Program;
