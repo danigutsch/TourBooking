1e121b621faabff321787cb06557f10e069285af
danigutsch <daniutsch@outlook.com>
2025-07-06 14:58:32 +0200

    Refactor API test fixtures and update endpoint references
    - Updated `ApiFixture` to use external alias for `Program`.
    - Introduced `WebFixture` with external alias and Kestrel setup.
    - Removed dependency on `TourBooking.Web.Contracts` in `TourTests`.
    - Changed endpoint reference in `TourTests` to `ToursApiEndpoints.GetTours`.

--- tests/TourBooking.Tests.Shared/ApiFixture.cs
+++ tests/TourBooking.Tests.Shared/ApiFixture.cs
@@ -1,7 +1,9 @@
+extern alias api;
+
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace TourBooking.Tests.Shared;
 
 [MustDisposeResource]
-public sealed class ApiFixture : WebApplicationFactory<Program>;
+public sealed class ApiFixture : WebApplicationFactory<api::Program>;
---
+++ tests/TourBooking.Tests.Shared/WebFixture.cs
@@ -0,0 +1,14 @@
+﻿extern alias web;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace TourBooking.Tests.Shared;
+
+[MustDisposeResource]
+public class WebFixture : WebApplicationFactory<web::Program>
+{
+    public WebFixture()
+    {
+        UseKestrel(0);
+    }
+}
